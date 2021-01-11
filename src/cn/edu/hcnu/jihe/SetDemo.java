091a240d1d2276619ef1b1cb5cfcd3d3f9e7c38e
liwei <7274561@qq.com>
2021-01-11 17:04:57 +0800

    List和Set、泛型

---
+++ src/cn/edu/hcnu/jihe/SetDemo.java
@@ -0,0 +1,23 @@
+package cn.edu.hcnu.jihe;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class SetDemo {
+    public static void main(String[] args) {
+        Set set=new HashSet();
+        set.add(1);
+        set.add("河池学院");
+        set.add(888.8F);
+        set.add(false);
+        set.add(1);
+
+        System.out.println("当前集合的元素个数："+set.size());
+
+        Iterator iterator=set.iterator();
+        while(iterator.hasNext()){
+            System.out.println("集合内的元素："+iterator.next());
+        }
+    }
+}
---
+++ src/cn/edu/hcnu/jihe/SetDemo01.java
@@ -0,0 +1,11 @@
+package cn.edu.hcnu.jihe;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetDemo01 {
+    public static void main(String[] args) {
+        Set<String> set=new HashSet<String>();
+        set.add("河池学院");
+    }
+}
